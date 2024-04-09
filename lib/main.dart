34083a4834ec1fd1ee58fa1aed70b15645abb8cb
anton <a.tashchilin@bk.ru>
2024-04-09 17:10:03 +0400

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import './models/database_provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import './screens/category_screen.dart';
+import './screens/expense_screen.dart';
+import './screens/all_expenses.dart';
+
+void main() async {
+
+  await initApp();
+  runApp(ChangeNotifierProvider(
+    create: (_) => DatabaseProvider(),
+    child: const MyApp(),
+  ));
+}
+
+
+Future<void> initApp() async {
+  if (Platform.isWindows || Platform.isLinux) {
+    sqfliteFfiInit();
+  }
+  databaseFactory = databaseFactoryFfi;
+  await initializeDateFormatting('ru', null);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: CategoryScreen.name,
+      routes: {
+        CategoryScreen.name: (_) => const CategoryScreen(),
+        ExpenseScreen.name: (_) => const ExpenseScreen(),
+        AllExpenses.name: (_) => const AllExpenses(),
+      },
+    );
+  }
+}
